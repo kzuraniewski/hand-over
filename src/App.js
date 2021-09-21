31d91e9ef82506b2e06fc625c3e0541e6f66d23d
nick-here <zuraniewski.karol@gmail.com>
2021-09-21 18:12:21 +0200

    routing + tailwind(?)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Home from './components/home/Home';
+
 function App() {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/'>
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;

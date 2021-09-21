c862951a291bda93e27da9655e48b6ebfdfbf684
nick-here <zuraniewski.karol@gmail.com>
2021-09-21 19:40:04 +0200

    routing fixed + not found page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './components/home/Home';
+import NotFound from './components/NotFound';
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='/'>
-                    <Home />
-                </Route>
+                <Route exact path='/' component={Home}></Route>
+                <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
     );

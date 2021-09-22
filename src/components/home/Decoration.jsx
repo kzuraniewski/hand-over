f2908c757d81b6fcda06ae63d3b81c09a69da140
nick-here <zuraniewski.karol@gmail.com>
2021-09-22 18:31:50 +0200

    decoration as react component + fixed size

---
+++ src/components/home/Decoration.jsx
@@ -0,0 +1,7 @@
+//TODO: css?
+import React from 'react';
+import decoration from '../../assets/Decoration.svg';
+
+export default function Decoration() {
+    return <img src={decoration} alt='decoration' className='decoration' />;
+}

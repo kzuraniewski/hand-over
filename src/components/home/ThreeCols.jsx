ae701d4f5f0b6b6958e104f8ede942d151b0d2ed
nick-here <zuraniewski.karol@gmail.com>
2021-09-22 15:07:31 +0200

    three cols section

--- src/components/home/ThreeCols.jsx
+++ src/components/home/ThreeCols.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
 
 export default function ThreeCols() {
-    return <></>;
+    return (
+        <div className='three-cols'>
+            <div className='three-cols__col'>
+                <div className='three-cols__number'>10</div>
+                <h3 className='three-cols__title'>oddanych worków</h3>
+                <p className='three-cols__description'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,
+                    assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor
+                    quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.
+                </p>
+            </div>
+            <div className='three-cols__col'>
+                <div className='three-cols__number'>5</div>
+                <h3 className='three-cols__title'>wspartych organizacji</h3>
+                <p className='three-cols__description'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,
+                    assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor
+                    quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.
+                </p>
+            </div>
+            <div className='three-cols__col'>
+                <div className='three-cols__number'>7</div>
+                <h3 className='three-cols__title'>zorganizowanych zbiórek</h3>
+                <p className='three-cols__description'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,
+                    assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor
+                    quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.
+                </p>
+            </div>
+        </div>
+    );
 }

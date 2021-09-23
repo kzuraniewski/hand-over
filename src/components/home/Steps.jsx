7397efb2a96a9a15304079843eabba4709d40f94
nick-here <zuraniewski.karol@gmail.com>
2021-09-23 19:06:38 +0200

    steps section

--- src/components/home/Steps.jsx
+++ src/components/home/Steps.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Decoration from './Decoration';
 import tshirt from '../../assets/Icon-1.svg';
 import bag from '../../assets/Icon-2.svg';
 import magnifier from '../../assets/Icon-3.svg';
 import loop from '../../assets/Icon-4.svg';
 
+//TODO
+// function Step({ src, title, desc }) {
+//     return (
+//         <div className='steps__col'>
+//             <img src={src} alt='' role='presentation' className='steps__icon' />
+//             <h2 className='steps__title'>{title}</h2>
+//             <Decoration small />
+//             <p className='steps__description'>{desc}</p>
+//         </div>
+//     );
+// }
+
 export default function Steps() {
     return (
-        <div className='steps'>
-            <h1 className='steps__title'>Wystarczą 4 proste kroki</h1>
+        <div className='content-group'>
+            <h1 className='content-group__title'>Wystarczą 4 proste kroki</h1>
 
-            <div className='decoration' />
+            <Decoration />
 
-            <div className='steps__cols'>
-                <div className='steps__col'>
-                    <img src={tshirt} alt='koszulka' className='steps__icon' />
-                    <h2 className='steps__title'>Wybierz rzeczy</h2>
-                    <p className='steps__description'>ubrania, zabawki, sprzęt i inne</p>
-                </div>
-                <div className='steps__col'>
-                    <img src={bag} alt='torba' className='steps__icon' />
-                    <h2 className='steps__title'>Spakuj je</h2>
-                    <p className='steps__description'>skorzystaj z worków na śmieci</p>
-                </div>
-                <div className='steps__col'>
-                    <img src={magnifier} alt='lupa' className='steps__icon' />
-                    <h2 className='steps__title'>Zdecyduj komu chcesz pomóc</h2>
-                    <p className='steps__description'>wybierz zaufane miejsce</p>
-                </div>
-                <div className='steps__col'>
-                    <img src={loop} alt='pętla' className='steps__icon' />
-                    <h2 className='steps__title'>Zamów kuriera</h2>
-                    <p className='steps__description'>kurier przyjedzie w dogodnym momencie</p>
-                </div>
+            <div className='steps'>
+                {[
+                    [tshirt, 'Wybierz rzeczy', 'ubrania, zabawki, sprzęt i inne'],
+                    [bag, 'Spakuj je', 'skorzystaj z worków na śmieci'],
+                    [magnifier, 'Zdecyduj komu chcesz pomóc', 'wybierz zaufane miejsce'],
+                    [loop, 'Zamów kuriera', 'kurier przyjedzie w dogodnym momencie'],
+                ].map(([src, title, desc]) => (
+                    <div className='steps__col'>
+                        <img src={src} alt='' role='presentation' className='steps__icon' />
+                        <h2 className='steps__title'>{title}</h2>
+                        <Decoration small />
+                        <p className='steps__description'>{desc}</p>
+                    </div>
+                ))}
             </div>
 
             <Link className='btn' to='/oddaj-rzeczy'>

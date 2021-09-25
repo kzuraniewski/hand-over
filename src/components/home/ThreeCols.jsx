b58f751cf7658a5f1dc8269eefa18eac1624f2f2
nick-here <zuraniewski.karol@gmail.com>
2021-09-25 14:30:50 +0200

    three cols content from external file

--- src/components/home/ThreeCols.jsx
+++ src/components/home/ThreeCols.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
+import colData from '../../data/threecols.data';
 
 export default function ThreeCols() {
     return (
         <div className='three-cols'>
-            <div className='three-cols__col'>
-                <div className='three-cols__number'>10</div>
-                <h3 className='three-cols__title'>oddanych worków</h3>
-                <p className='three-cols__description'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,
-                    assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor
-                    quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.
-                </p>
-            </div>
-            <div className='three-cols__col'>
-                <div className='three-cols__number'>5</div>
-                <h3 className='three-cols__title'>wspartych organizacji</h3>
-                <p className='three-cols__description'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,
-                    assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor
-                    quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.
-                </p>
-            </div>
-            <div className='three-cols__col'>
-                <div className='three-cols__number'>7</div>
-                <h3 className='three-cols__title'>zorganizowanych zbiórek</h3>
-                <p className='three-cols__description'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,
-                    assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor
-                    quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.
-                </p>
-            </div>
+            {colData.map(({ number, title, description }) => (
+                <div className='three-cols__col'>
+                    <div className='three-cols__number'>{number}</div>
+                    <h3 className='three-cols__title'>{title}</h3>
+                    <p className='three-cols__description'>{description}</p>
+                </div>
+            ))}
         </div>
     );
 }
---
+++ src/data/threecols.data.js
@@ -0,0 +1,22 @@
+const colData = [
+    {
+        number: 10,
+        title: 'oddanych worków',
+        description:
+            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,        assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.',
+    },
+    {
+        number: 5,
+        title: 'wspartych organizacji',
+        description:
+            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,        assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.',
+    },
+    {
+        number: 7,
+        title: 'zorganizowanych zbiórek',
+        description:
+            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus,        assumenda, vitae nihil corrupti in tempora obcaecati expedita fugit quis dolor quibusdam amet. Ut voluptas, expedita nam perspiciatis tempore ipsa.',
+    },
+];
+
+export default colData;

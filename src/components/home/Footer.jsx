2db0b99fa154654e6d421e2c5143415c4b126c4b
nick-here <zuraniewski.karol@gmail.com>
2021-09-27 17:56:39 +0200

    footer jsx

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function Footer() {
-    return <></>;
+    return (
+        <footer className='footer'>
+            {/* TODO */}
+            <div />
+
+            <div className='footer__text'>Copyright by Coders Lab</div>
+
+            <div className='footer__icon-wrapper'>
+                <a href='#' className='footer__icon'>
+                    <i className='fab fa-facebook'></i>
+                </a>
+                <a href='#' className='footer__icon'>
+                    <i className='fab fa-instagram'></i>
+                </a>
+            </div>
+        </footer>
+    );
 }

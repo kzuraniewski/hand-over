2e46841247c1a4db1fe65d6abd642756017564e0
nick-here <zuraniewski.karol@gmail.com>
2021-09-27 16:54:58 +0200

    Form component

--- src/components/utility/Form.jsx
+++ src/components/utility/Form.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+const defaultErrorMessage = 'To pole nie może być puste';
+
+/**
+ * @param {string} value
+ * @returns {boolean}
+ */
+const defaultValidation = value => !value.length;
+
+/**
+ * Returns whether the given input is valid
+ * @param {object} ref
+ * @param {(value: string) => boolean} validation
+ */
+// const useValidation = ([ref], validation) => {
+//     const mount = useRef(true);
+//     const [error, setError] = useState(false);
+
+//     useEffect(() => {
+//         if (mount) return;
+
+//         setError(validation(ref.current.value));
+//     }, [ref, validation, ref.current?.value]);
+
+//     return [error];
+// };
 
 /**
  * @param {object} props
  * @param {string} props.label
  * @param {string} [props.placeholder = '']
+ * @param {string} [props.errorMessage = '']
+ * @param {(value: string) => boolean} [props.validation = defaultValidation]
  */
-export function FormInput({ label, placeholder = '' }) {
+export function FormInput({
+    label,
+    placeholder = '',
+    validation = defaultValidation,
+    errorMessage = defaultErrorMessage,
+}) {
+    // const ref = useRef(null);
+    // const [showErrorMessage] = useValidation([ref], validation);
+    const showErrorMessage = false;
+
     return (
         <label className='form__label'>
             {label}
             <input type='text' className='form__input' placeholder={placeholder} />
+            {showErrorMessage && <div className='form__error-message'>{errorMessage}</div>}
         </label>
     );
 }
@@ -18,18 +56,31 @@ export function FormInput({ label, placeholder = '' }) {
  * @param {object} props
  * @param {string} props.label
  * @param {string} [props.placeholder = '']
+ * @param {string} [props.errorMessage = defaultErrorMessage]
+ * @param {(value: string) => boolean} [props.validation = defaultValidation]
  */
-export function FormTextarea({ label, placeholder = '' }) {
+export function FormTextarea({
+    label,
+    placeholder = '',
+    validation = defaultValidation,
+    errorMessage = defaultErrorMessage,
+}) {
+    // const ref = useRef(null);
+    // const [showErrorMessage] = useValidation([ref], validation);
+    const showErrorMessage = false;
+
     return (
         <label className='form__label'>
             {label}
             <textarea
+                // ref={ref}
                 className='form__input'
                 name={label}
                 cols={30}
                 rows={7}
                 placeholder={placeholder}
             />
+            {showErrorMessage && <div className='form__error-message'>{errorMessage}</div>}
         </label>
     );
 }

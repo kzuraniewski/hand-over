35187167cfa8dce0a227b596faa7562e10dc0581
nick-here <zuraniewski.karol@gmail.com>
2021-09-27 14:33:26 +0200

    children wrapped in content group

--- src/components/utility/ImageGroup.jsx
+++ src/components/utility/ImageGroup.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import ContentGroup from './ContentGroup';
 
-export default function ImageGroup({ src, alt, overlay = false, children }) {
+export default function ImageGroup({ title, src, alt, overlay = false, children }) {
     return (
         <div className={`grid image-group${overlay ? ' image-group--overlay' : ''}`}>
             <img className='image-group__image' src={src} alt={alt} />
 
-            <div className='image-group__content'>{children}</div>
+            <div className='image-group__content'>
+                <ContentGroup title={title}>{children}</ContentGroup>
+            </div>
         </div>
     );
 }

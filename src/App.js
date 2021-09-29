d36595de3c528eccd296130d8568265beae9908a
nick-here <zuraniewski.karol@gmail.com>
2021-09-29 17:11:55 +0200

    login structure + routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/Header';
 import NotFound from './components/NotFound';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
     return (
@@ -9,6 +11,8 @@ function App() {
             <Header />
             <Switch>
                 <Route exact path='/' component={Home}></Route>
+                <Route exact path='/logowanie' component={Login}></Route>
+                <Route exact path='/rejestracja' component={Register}></Route>
                 <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import ContentGroup from '../utility/ContentGroup';
 
 export default function Login() {
-    return <></>;
+    return (
+        <section className='user-auth'>
+            <ContentGroup title='Zaloguj się'>
+                <form className='form user-auth__form'>
+                    <input type='text' className='form__input' />
+                    <input type='text' className='form__input' />
+                </form>
+
+                <div className='user-auth__btn-wrapper'>
+                    <button className='btn btn--small btn--lowercase btn--borderless'>
+                        Załóż konto
+                    </button>
+                    <button className='btn btn--small btn--lowercase'>Zaloguj się</button>
+                </div>
+            </ContentGroup>
+        </section>
+    );
 }

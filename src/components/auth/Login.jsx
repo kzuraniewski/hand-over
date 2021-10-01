19bddb95d8ee73f3213321b017a066b9f7c63b91
nick-here <zuraniewski.karol@gmail.com>
2021-10-01 05:32:02 +0200

    login validation + register button working

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,29 +1,72 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ContentGroup from '../utility/ContentGroup';
+import { validatePassword, validateEmail } from '../../validation';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+    const [showError, setShowError] = useState({ email: false, password: false });
+    const inputRefs = {
+        email: useRef(null),
+        password: useRef(null),
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        setShowError(() => {
+            const [emailValue, passwordValue] = Object.values(inputRefs).map(
+                ref => ref.current.value
+            );
+
+            return {
+                email: !validateEmail(emailValue),
+                password: !validatePassword(passwordValue),
+            };
+        });
+    };
+
     return (
         <section className='user-auth'>
             <ContentGroup title='Zaloguj się'>
-                <form className='form user-auth__form'>
-                    <label className='form__label'>
-                        Email
-                        <input type='text' className='form__input' />
-                    </label>
-                    <label className='form__label'>
-                        Hasło
-                        <input type='text' className='form__input' />
-                    </label>
-                </form>
+                <form className='form' onSubmit={e => handleSubmit(e)}>
+                    {/* input */}
+                    <div className='user-auth__input-wrapper'>
+                        <label className='form__label'>
+                            Email
+                            <input ref={inputRefs.email} type='text' className='form__input' />
+                            {showError.email && (
+                                <div className='form__error-message'>
+                                    Podany email jest nieprawidłowy!
+                                </div>
+                            )}
+                        </label>
+                        <label className='form__label'>
+                            Hasło
+                            <input ref={inputRefs.password} type='text' className='form__input' />
+                            {showError.password && (
+                                <div className='form__error-message'>
+                                    Podane hasło jest za krótkie!
+                                </div>
+                            )}
+                        </label>
+                    </div>
 
-                <div className='user-auth__btn-wrapper'>
-                    <button className='user-auth__btn btn btn--small btn--lowercase btn--borderless'>
-                        Załóż konto
-                    </button>
-                    <button className='user-auth__btn btn btn--small btn--lowercase'>
-                        Zaloguj się
-                    </button>
-                </div>
+                    {/* buttons  */}
+                    <div className='user-auth__btn-wrapper'>
+                        <Link
+                            to='/rejestracja'
+                            className='user-auth__btn btn btn--small btn--lowercase btn--borderless'
+                        >
+                            Załóż konto
+                        </Link>
+
+                        <input
+                            className='user-auth__btn btn btn--small btn--lowercase'
+                            type='submit'
+                            value='Zaloguj się'
+                        />
+                    </div>
+                </form>
             </ContentGroup>
         </section>
     );

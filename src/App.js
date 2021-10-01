f398d09700f8e6e7496edce7c49d01779efacc3e
nick-here <zuraniewski.karol@gmail.com>
2021-10-01 10:52:28 +0200

    logout subpage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import NotFound from './components/NotFound';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import LogOut from './components/auth/LogOut';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route exact path='/' component={Home}></Route>
                 <Route exact path='/logowanie' component={Login}></Route>
                 <Route exact path='/rejestracja' component={Register}></Route>
+                <Route exact path='/wylogowanie' component={LogOut}></Route>
                 <Route component={NotFound} />
             </Switch>
         </BrowserRouter>

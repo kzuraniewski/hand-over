42ebf6807c3f3977101ed1916fba995b695484b2
nick-here <zuraniewski.karol@gmail.com>
2021-10-05 10:49:29 +0200

    hand-over steps section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NotFound from './components/NotFound';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import LogOut from './components/auth/LogOut';
+import HandOver from './components/hand-over/HandOver';
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route exact path='/logowanie' component={Login}></Route>
                 <Route exact path='/rejestracja' component={Register}></Route>
                 <Route exact path='/wylogowano' component={LogOut}></Route>
+                <Route exact path='/oddaj-rzeczy' component={HandOver}></Route>
                 <Route component={NotFound} />
             </Switch>
         </BrowserRouter>

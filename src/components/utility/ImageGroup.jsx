ee93ec8adfe8c4a0c5ce19260e7718943f1e49e1
nick-here <zuraniewski.karol@gmail.com>
2021-10-05 10:49:02 +0200

    image group fullscreen modifier

--- src/components/utility/ImageGroup.jsx
+++ src/components/utility/ImageGroup.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import ContentGroup from './ContentGroup';
 
-export default function ImageGroup({ title, src, alt, overlay = false, children }) {
+/**
+ *
+ * @param {object} props
+ * @param {string} props.title
+ * @param {string} props.src
+ * @param {string} props.alt
+ * @param {boolean} [props.overlay = false]
+ * @param {boolean} [props.fullscreen = false]
+ * @param {*} props.children
+ */
+export default function ImageGroup({
+    title,
+    src,
+    alt,
+    overlay = false,
+    fullscreen = false,
+    children,
+}) {
     return (
-        <div className={`grid image-group${overlay ? ' image-group--overlay' : ''}`}>
+        <div
+            className={`grid image-group${overlay ? ' image-group--overlay' : ''}${
+                fullscreen ? ' image-group--fullscreen' : ''
+            }`}
+        >
             <img className='image-group__image' src={src} alt={alt} />
 
-            <div className='image-group__content'>
+            <div className='image-group__content-wrapper'>
                 <ContentGroup title={title}>{children}</ContentGroup>
             </div>
         </div>

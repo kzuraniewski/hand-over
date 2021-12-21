61c53d56bab33886e5430931b908674bbc0e9319
nick-here <zuraniewski.karol@gmail.com>
2021-12-21 14:55:09 +0100

    content-group children no longer inside a div

--- src/components/utility/ContentGroup.jsx
+++ src/components/utility/ContentGroup.jsx
@@ -3,39 +3,39 @@ import decoration from '../../assets/Decoration.svg';
 
 /**
  * @param {object} props
- * @param {string | string[]} props.title
+ * @param {string | string[]} props.title - the title of the content group
  * @param {object} props.children
  */
 export default function ContentGroup({ title, children }) {
-    /**
-     * If the title is an array, then returns it converted to JSX with <br />'s
-     * @returns {JSX.Element[] | string | string[]}
-     */
-    const convertedTitle = () => {
-        if (Array.isArray(title) && title.length > 1) {
-            return title.map((el, index) => (
-                <React.Fragment key={index}>
-                    <br />
-                    {el}
-                </React.Fragment>
-            ));
-        }
+	/**
+	 * If the title is an array, then returns it converted to JSX with <br />'s
+	 * @returns {JSX.Element[] | string | string[]}
+	 */
+	const convertedTitle = () => {
+		if (Array.isArray(title) && title.length > 1) {
+			return title.map((el, index) => (
+				<React.Fragment key={index}>
+					<br />
+					{el}
+				</React.Fragment>
+			));
+		}
 
-        return title;
-    };
+		return title;
+	};
 
-    return (
-        <div className={`content-group`}>
-            <h1 className='content-group__title'>{convertedTitle()}</h1>
+	return (
+		<div className={`content-group`}>
+			<h1 className='content-group__title'>{convertedTitle()}</h1>
 
-            <img
-                src={decoration}
-                alt=''
-                role='presentation'
-                className='content-group__decoration'
-            />
+			<img
+				src={decoration}
+				alt=''
+				role='presentation'
+				className='content-group__decoration'
+			/>
 
-            <div>{children}</div>
-        </div>
-    );
+			{children}
+		</div>
+	);
 }
